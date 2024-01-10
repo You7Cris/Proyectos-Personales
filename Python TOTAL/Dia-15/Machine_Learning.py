24b5fc9c19a6c6aa85405b5effb894c6ccd8dccf
Cristian Gonzalez Arango <31244463+You7Cris@users.noreply.github.com>
2024-01-10 16:30:34 -0500

    Dia 15 - 16 (Curso Python Total) Finalizado

---
+++ Python TOTAL/Dia-15/Machine_Learning.py
@@ -0,0 +1,19 @@
+# numpy - operciones numericas
+# pandas - analizar datasets
+# matplotlib - visualizar datos
+
+# Rama de la inteligencia artificial que consiste en entrenar la computadora
+
+# Google Colab
+
+# numpy - Libreria estandar para realizar operaciones numericas a gran escala
+# https://colab.research.google.com/drive/1FcXuSvXiWJoupZZDSUkpkc24xbRqZTWA?usp=sharing
+
+# panda - excel de python
+# https://colab.research.google.com/drive/1c16dIDFdyrak3RF948Ji-3isPASwLjmC?usp=sharing
+
+# Matplotlib 
+# https://colab.research.google.com/drive/1WxNqnDXvsJefPtW3V4YUWFjIS6pIjPhF?usp=sharing
+
+# Aprendizaje supervisado
+# sci- kit learn
---
+++ Python TOTAL/Dia-16/entornos_virtuales.py
@@ -0,0 +1,23 @@
+# py -m pip install virtualenv
+# pip freeze # ver todos los paquetes instalados en mi PC
+
+### Instalando desde Git Bash 
+
+# py -m virtualenv p1
+# source .\p1\Scripts\activate
+# pip freeze
+# deactivate
+
+#### Instalacion de Django
+# pip install Django
+# pip freeze
+# mkdir src
+# cd src
+# django-admin startprojects proyecto
+# python manage.py runserver
+# python manage.py migrate
+# python manage.py createsuperuser
+# winpty python manage.py createsuperuser
+# python manage.py startapp base
+# pyhton manage.py makemigrations
+# python manage.py migrate
