f510089602a1f2a602926a48625cc959d3fce721
Cristian Gonzalez Arango <31244463+You7Cris@users.noreply.github.com>
2024-04-04 19:51:12 -0500

    Actualizaion de archivos y paqquetes

--- football-data.org/api/services.py
+++ football-data.org/api/services.py
@@ -2,7 +2,8 @@ import requests
 
 def get_username():
     url = "https://api.football-data.org/v4/matches"
-    headers = { 'X-Auth-Token' : '7ea9c80b36364478940b361b2dcaa23f' }
+    headers = { 'X-Auth-Token' : '7ea9c80b36364478940b361b2dcaa23f'
+}
     data = requests.get(url, headers=headers)
     if data.status_code == 200:
         return data.json()['matches']
